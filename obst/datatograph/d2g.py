ddf93be791a05d6ce7d909a847170e4027ff2f02
Valentin Roland <valentin@vroland.de>
2019-11-09 22:14:37 +0100

    use pre-computed graphs

--- obst/datatograph/d2g.py
+++ obst/datatograph/d2g.py
@@ -10,47 +10,64 @@ from openrouteservice import Client
 from openrouteservice.distance_matrix import distance_matrix
 from openrouteservice.directions import directions
 
-Graph = namedtuple("Graph", ["nodes", "edges", "distance_matrix"])
+Graph = namedtuple("Graph", ["nodes", "distance_matrix", "name"])
 Node  = namedtuple("Node", ["id", "location", "metadata"])
-Edge  = namedtuple("Edge", ["u", "v"])
+
+graphs_dir = "../../graphs/"
+
+def file_to_graph(path):
+    name = path.split("/")[-1].split(".")[0]
+
+    raw = json.load(open(path))["features"]
+
+    nodes = []
+    for feature in raw:
+        location = feature["geometry"]["coordinates"]
+        metadata = feature["properties"]
+        ident = name + str(metadata["id"])
+        node = Node(ident, location, metadata)
+        nodes.append(node)
+
+    # Api Client with custom base URL
+    client = Client(base_url="http://localhost:8080/ors/")
+
+    def dist_mtrx(nodes):
+        nodes = [node.location for node in nodes]
+        route = client.distance_matrix(locations=nodes)  # remove list splice for big matrices
+        dm = [list(map(lambda x: int(x/60), dur)) for dur in route["durations"]]
+        return dm
+
+    def get_route(route):
+        for coord_pair in zip(route, route[1:]):
+            section = directions(coord_pair)
+        # append sections to whole route
+
+        return
+
+    def dist(u, v):
+        return int(100*math.sqrt((u.location[0] - v.location[0])**2 + (u.location[1] - v.location[1])**2)) + 1
+
+    dm = dist_mtrx(nodes)
+    # distance_matrix = [[dist(u,v) for v in nodes] for u in nodes]
+    graph = Graph(nodes, dm, name)
+    print ("loaded", path, "(", name, ")", "with", len(graph.nodes), "nodes!")
+    return graph
 
 
-path = "../../geojson/Sehenswürdigkeiten.geojson"
-name = path.split("/")[-1].split(".")[0]
+def load_graph(raw):
+    obj = json.loads(raw)
+    nodes = [Node(*n) for n in obj[0]]
+    graph = Graph(nodes, *obj[1:])
+    return graph
 
-raw = json.load(open(path))["features"]
+paths = [
+        "../../geojson/bereinigte_Toiletten.geojson",
+        "../../geojson/Zukunftsstadt.geojson",
+        "../../geojson/sehenswuerdigkeiten.geojson"
+]
 
-nodes = []
-for feature in raw:
-    location = feature["geometry"]["coordinates"]
-    metadata = feature["properties"]
-    ident = name + str(metadata["id"])
-    node = Node(ident, location, metadata)
-    nodes.append(node)
-
-# Api Client with custom base URL
-client = Client(base_url="http://localhost:8080/ors/")
-
-def dist_mtrx(nodes):
-    nodes = [node.location for node in nodes]
-    route = client.distance_matrix(locations=nodes)  # remove list splice for big matrices
-    dm = [list(map(lambda x: int(x/60), dur)) for dur in route["durations"]]
-    return dm
-
-def get_route(tsp_route):
-    Route = namedtuple("Route", "sections coordinates")
-    sections, coordinates = [], []
-    for coord_pair in zip(tsp_route, tsp_route[1:]):
-        section = directions(coord_pair)
-        sections.append(section)
-        coordinates.extend(section["geometry"]["coordinates"])
-    return Route(sections, coordinates)
-
-def dist(u, v):
-    return int(100*math.sqrt((u.location[0] - v.location[0])**2 + (u.location[1] - v.location[1])**2)) + 1
-
-edges = [Edge(u, v) for u in nodes for v in nodes]
-dm = dist_mtrx(nodes)
-# distance_matrix = [[dist(u,v) for v in nodes] for u in nodes]
-graph = Graph(nodes, edges, dm)
-print (dm)
+if __name__ == "__main__":
+    for path in paths:
+        graph = file_to_graph(path)
+        with open(graphs_dir + graph.name + ".json", "w") as f:
+            f.write(json.dumps(graph))
